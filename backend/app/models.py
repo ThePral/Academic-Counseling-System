1cc5e4a02fa85b087a425222eef662dbe4209164
Pourya-Alizadeh <pouryaalizadeh97@gmail.com>
2025-07-31 01:20:33 +0330

    add notifications to models

--- backend/app/models.py
+++ backend/app/models.py
@@ -221,4 +221,13 @@ class Feedback(Base):
     counselor = relationship("Counselor", back_populates="feedbacks")
 
 
+# ----- NOTIFICATIONS -----
 
+class Notification(Base):
+    __tablename__ = "notifications"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, index=True)
+    message = Column(String, nullable=False)
+    read = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
