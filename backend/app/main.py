a0de45a881c0a620d2667ae74e530c5b155b7f6a
Pourya-Alizadeh <pouryaalizadeh97@gmail.com>
2025-08-03 14:13:31 +0330

    add CORS Middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from .database import Base, engine
 from app.routers import  authentication,students, counselors, appointments, time_slots, public, reset_password, study_plan, notifications, admin
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Academic Counseling API")
 
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(authentication.router)
 app.include_router(students.router)
